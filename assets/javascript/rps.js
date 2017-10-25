76fa5744839dcf6312788cd1edc35250e9b8abd8
matlittle <matlittle727@gmail.com>
2017-10-25 11:00:07 -0700

    Function created to prompt for a username when join is clicked

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -6,8 +6,28 @@ var database = firebase.database();
 
 // when user clicks an available spot, promt them for a username
 	// after entering a username, add that user to firebase rt db
-	// show waiting until another user joins. 
+	// show waiting until another user joins.
+
+function playerJoining(el) {
+	var player = $(el).attr("id");
+
+	var username = usernamePrompt();
+}
+
+function usernamePrompt(){
+	var modal = $("<div class='modal' id='username-prompt'>");
+	var label = $("<label id='username-label'>").text("Enter a Username");
+	var input = $("<input type='text' id='username-input'>");
+	var btn = $("<input type='button' id='username-btn' value='Join'>");
+
+	$(modal).append(label, input, btn);
+	$("#wrapper").append(modal);
+}
 
 // when second user joins, prompt both users with a rps choice. 
 	// after both users click, determine winner based on choices.
 	// increment user's score based on win/loss
+
+
+
+$(document).on("click", ".join-btn", playerJoining(this));
