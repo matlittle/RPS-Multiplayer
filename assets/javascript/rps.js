4312234ab504806ebe4474c552645c4e4a26f196
matlittle <matlittle727@gmail.com>
2017-10-25 10:13:25 -0700

    Created basic html layout and starting elements

---
+++ assets/javascript/rps.js
@@ -0,0 +1,13 @@
+// Get reference to the Firebase realtime database service
+var database = firebase.database();
+
+// initialize page, allow a user to "join" one of two available spots, and include a chat window at the bottom of the page
+
+
+// when user clicks an available spot, promt them for a username
+	// after entering a username, add that user to firebase rt db
+	// show waiting until another user joins. 
+
+// when second user joins, prompt both users with a rps choice. 
+	// after both users click, determine winner based on choices.
+	// increment user's score based on win/loss
