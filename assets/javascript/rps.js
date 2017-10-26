13b95c40124f14d7741f377cdd35a3787974ae34
matlittle <matlittle727@gmail.com>
2017-10-25 20:24:16 -0500

    Added function to add user data to page

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,6 +1,7 @@
 // Get reference to the Firebase realtime database service
 var database = firebase.database();
-console.log(database);
+
+var curr = {};
 
 // initialize page, allow a user to "join" one of two available spots, and include a chat window at the bottom of the page
 
@@ -9,18 +10,10 @@ console.log(database);
 	// after entering a username, add that user to firebase rt db
 	// show waiting until another user joins.
 
-function playerJoining(el) {
-	//var player = $(el).attr("id");
 
-	usernamePrompt(el);
-
-	$("#username-btn").click(function() {
-		addPlayer($("#username-input").val().trim())
-	});
-}
 
 function usernamePrompt(el){
-	var label = $("<label id='username-label'>").text("Enter a Username");
+	var label = $("<label id='username-label'>").text("Enter your Username");
 	var input = $("<input type='text' id='username-input'>");
 	var btn = $("<input type='button' id='username-btn' value='Join'>");
 
@@ -29,28 +22,54 @@ function usernamePrompt(el){
 	$(parent).append(label, "<br>", input, btn);
 }
 
-function addPlayer(name) {
+function playerJoining(el) {
+	//var player = $(el).attr("id");
 
-	database.ref(`/users/${name}`).once("value").then(function(data) {
-		if(data.val()){
-			loadPlayerData(data.val())
-		}else {
-			writePlayerData(name);
+	usernamePrompt(el);
+
+	var playerNum = $(el).attr("data-player");
+
+	$("#username-btn").click(function() {
+		var input = $("#username-input").val().trim();
+
+		if(input.length > 0) {
+			$(`#player-${playerNum} > div`).empty();
+			checkIfNewPlayer(input, playerNum);
 		}
 	});
-	
 }
 
-function writePlayerData(user) {
-	database.ref(`/users/${user}`).set({
+function checkIfNewPlayer(name, num) {
+	database.ref(`/users/${name}`).once("value").then(function(data) {
+		if(data.val()){
+			addPlayerToGame(data.val(), num);
+		}else {
+			writePlayerData(name, num);
+		}
+	});
+}
+
+function writePlayerData(user, num) {
+	var playerData = {
 		username: user,
 		wins: 0,
 		losses: 0
-	});
+	}
+
+	database.ref(`/users/${user}`).set(playerData);
+
+	addPlayerToGame(playerData, num);
 }
 
-function loadPlayerData(user) {
-	console.log(user);
+function addPlayerToGame(userObj, num) {
+	var header = $("<h2 class='player-head'>").text(userObj.username);
+	var score = $("<p class='score'>")
+	var wins = $("<span class='wins'>").text(`Wins: ${userObj.wins}`);
+	var losses = $("<span class='losses'>").text(`Losses: ${userObj.losses}`);
+
+	$(score).append(wins, losses);
+
+	$(`#player-${num} > div`).append(header, score);
 }
 
 // when second user joins, prompt both users with a rps choice. 
